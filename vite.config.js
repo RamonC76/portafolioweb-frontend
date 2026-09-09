a56f027d6fc084deb355568f44c23b7fb2bc9471
Ramón Corona <ramon.r.corona76@gmail.com>
2026-09-08 20:40:05 -0600

    Fixing little bugs

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react()],
+    base: '/portafolioweb-frontend/',
     server: {
         watch: {
             // Ignorar la carpeta oculta de Visual Studio para evitar el error EBUSY
